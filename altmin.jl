42d1e37444ac23dd477eb5061ece0793d0257e2f
Deep Majumder <deep.majumder2019@gmail.com>
2021-11-01 00:54:01 +0530

    Damned code!

--- altmin.jl
+++ altmin.jl
@@ -1,12 +1,93 @@
 module Altmin
 
-import Flux
+include("models.jl")
 
-function mark_code_mods(model, module_types=[])
-    if size(module_types) == 0
-        module_types = [Flux.Conv, Flux.Dense, Flux.BatchNorm]
+import Flux
+using Statistics: mean
+
+struct Flatten
+end
+
+function linearize_tensor(tensor)
+    dims = size(tensor)
+    compressed_len = reduce(*, dims[1:ndims(tensor)-1]; init=1)
+    reshape(tensor, (compressed_len, size(tensor, ndims(tensor))))
+end
+
+@Flux.functor Flatten
+(m::Flatten)(x) = reshape(x, :, size(x, ndims(x)))
+
+function compute_code_loss(codes, nmod, lin, loss_fn, codes_target, mu, lambda_c)
+    output = lin(nmod(codes))
+    loss = (1 / mu) * loss_fn(output) + Flux.mse(codes_target, output)
+    if lambda_c > 0.0
+        loss += (lambda_c / mu) * mean(abs.(codes))
+    end
+    return loss
+end
+
+function mark_code_mods(model)
+    module_types = [Models.ConvMod, Models.DenseMod, Models.BatchNormMod, Models.LinMod]
+    for mod in Iterators.flatten((model.features_vec, model.classifiers_vec))
+        if any(t -> isa(mod, t), module_types)
+            mod.has_codes = true
+        end
+    end
+end
+
+struct AltMinModel{C}
+    model::C
+    n_inputs::Union{Int32, Nothing}
+end
+
+function get_mods(model, optimizer, scheduler, data_parallel=False)
+    mark_code_mods(model)
+
+    model_mods::Vector{Any} = []
+    nmod::Vector{Any} = []
+    lmod::Vector{Any} = []
+
+    for m in Iterators.flatten((model.features_vec, [Models.Flatten()], model.classifier_vec))
+        if m.has_codes
+            nmod = insert_mod(model_mods, nmod, false)
+            push!(lmod, m)
+        else
+            lmod = insert_mod(model_mods, lmod, true)
+            push!(nmod, m)
+        end
     end
 
+    insert_mod(model_mods, nmod, false)
+    insert_mod(model_mods, lmod, true)
+
+    id_codes = [i for (i, m) in Iterators.enumerate(model_mods) if m.has_codes]
+
+    model_tmp = model_mods[1:id_codes[end - 1]]
+    push!(model_tmp, model_mods[id_codes[end - 1]+1:end])
+    model_tmp[end].has_codes = false
+    model_mods = model_tmp
+
+    for m in model_mods
+        if m.has_codes
+            m.optimizer = optimizer
+            # Scheduler?
+        end
+        # Last layer?
+    end
+
+    # Data parallel?
+    return AltMinModel(Flux.Chain(model_mods...), )
+end
+
+function insert_mod(model_mods, mods, has_codes)
+    if length(mod) == 1
+        push!(model_mods, mods[0])
+        model_mods[end].has_codes = has_codes
+    elseif length(mod) > 1
+        append!(model_mods, mods)
+        model_mods[end].has_codes = has_codes
+    end
+    return []
 end
 
 end
--- models.jl
+++ models.jl
@@ -3,36 +3,74 @@ module Models
 import Flux
 using Flux: cpu, gpu
 using Printf
+using CUDA
+
+struct DenseMod{C}
+    classifier::C
+    has_codes::Union{Bool, Nothing}
+end
+
+DenseMod(n_inputs, n_outputs, bias=true) = DenseMod(
+    Flux.Dense(n_inputs, n_outputs, bias=bias),
+    nothing
+)
+
+Flux.@functor DenseMod
+(m::DenseMod)(x) = m.classifier(x)
+
+struct BatchNormMod{C}
+    classifier::C
+    has_codes::Union{Bool, Nothing}
+end
+
+BatchNormMod(n_outputs) = BatchNormMod(
+    Flux.BatchNorm(Int(n_outputs)),
+    nothing
+)
+
+Flux.@functor BatchNormMod
+(m::BatchNormMod)(x) = m.classifier(x)
 
 struct LinMod{C}
     classifier::C
+    n_inputs::Int32
+    has_codes::Union{Bool, Nothing}
 end
 
 function LinMod(n_inputs, n_outputs; bias=false, batchnorm=false)
-    layers::Vector{Any} = [Flux.Dense(n_inputs, n_outputs, bias=bias)]
+    layers::Vector{Any} = [DenseMod(n_inputs, n_outputs, bias)]
     if batchnorm
-        push!(layers, Flux.BatchNorm(Int(n_outputs)))
+        push!(layers, BatchNormMod(Int(n_outputs)))
     end
-    return LinMod(Flux.Chain(layers...) |> gpu)
+    return LinMod(Flux.Chain(layers...) |> gpu, n_inputs, nothing)
 end
 
 Flux.@functor LinMod
 (m::LinMod)(x) = m.classifier(x)
 
+struct Relu
+    has_codes::Union{Bool, Nothing}
+end
+
+Relu() = Relu(nothing)
+
+Flux.@functor Relu
+(m::Relu)(x) = Flux.relu.(x)
+
 struct FFNet{C}
     features_vec::Vector{Any}
     classifier_vec::Vector{Any}
     classifier::C
+    n_inputs::Int32
 end
 
-function FFNet(;n_inputs, n_hiddens, n_hidden_layers=Int32(2), n_outputs=Int32(10), nlin=Flux.relu, bias=false, batchnorm=false)
-    nlin_arr(x) = nlin.(x)
-    layers::Vector{Any} = [LinMod(n_inputs, n_hiddens, bias=bias, batchnorm=batchnorm), nlin_arr]
+function FFNet(;n_inputs, n_hiddens, n_hidden_layers=Int32(2), n_outputs=Int32(10), bias=false, batchnorm=false)
+    layers::Vector{Any} = [LinMod(n_inputs, n_hiddens, bias=bias, batchnorm=batchnorm), Relu()]
     for i = 1:n_hidden_layers
-        push!(layers, LinMod(n_hiddens, n_hiddens, bias=bias, batchnorm=batchnorm), nlin_arr)
+        push!(layers, LinMod(n_hiddens, n_hiddens, bias=bias, batchnorm=batchnorm), Relu())
     end
-    push!(layers, Flux.Dense(n_hiddens, n_outputs))
-    return FFNet([], layers, Flux.Chain(layers...) |> gpu)
+    push!(layers, DenseMod(n_hiddens, n_outputs))
+    return FFNet([], layers, Flux.Chain(layers...) |> gpu, n_inputs)
 end
 
 Flux.@functor FFNet
@@ -45,45 +83,72 @@ struct LeNet{F, C}
     classifier::C
 end
 
+struct ConvMod{C}
+    classifier::C
+    has_codes::Union{Bool, Nothing}
+end
+
+ConvMod(size, in, out, bias) = ConvMod(
+    Flux.Conv(size, in => out; bias=bias),
+    nothing
+)
+
+Flux.@functor ConvMod
+(m::ConvMod)(x) = m.classifier(x)
+
+struct MaxPoolMod{C}
+    classifier::C
+    has_codes::Union{Bool, Nothing}
+end
+
+MaxPoolMod(size) = MaxPoolMod(
+    Flux.MaxPool(size),
+    nothing
+)
+
+Flux.@functor MaxPoolMod
+(m::MaxPoolMod)(x) = m.classifier(x)
+
 function LeNet(;num_input_channels=3, num_classes=10, window_size=32, bias=true)
-    relu(x) = Flux.relu.(x)
-
     features_vec::Vector{Any} = [
-        Flux.Conv((5, 5), num_input_channels => 6; bias=bias),
-        relu,
-        Flux.MaxPool((2, 2)),
+        ConvMod((5, 5), num_input_channels, 6, bias),
+        Relu(),
+        MaxPoolMod((2, 2)),
 
-        Flux.Conv((5, 5), 6 => 16, bias=bias),
-        relu,
-        Flux.MaxPool((2, 2))
+        ConvMod((5, 5), 6, 16, bias),
+        Relu(),
+        MaxPoolMod((2, 2))
     ]
     features = Flux.Chain(features_vec...) |> gpu
 
     inp_size = 16 * (((window_size - 4) ÷ 2 - 4) ÷ 2) ^ 2
     classifier_vec::Vector{Any} = [
-        Flux.Dense(inp_size, 120, bias=bias),
-        relu,
-        Flux.Dense(120, 84, bias=bias),
-        relu,
-        Flux.Dense(84, num_classes, bias=bias)
+        DenseMod(inp_size, 120, bias),
+        Relu(),
+        DenseMod(120, 84, bias),
+        Relu(),
+        DenseMod(84, num_classes, bias)
     ]
     classifier = Flux.Chain(classifier_vec...) |> gpu
 
     return LeNet(features_vec, classifier_vec, features, classifier)
 end
 
-function linearize_tensor(tensor)
-    dims = size(tensor)
-    compressed_len = reduce(*, dims[1:ndims(tensor)-1]; init=1)
-    reshape(tensor, (compressed_len, size(tensor, ndims(tensor))))
-end
-
 Flux.@functor LeNet
 function (m::LeNet)(x) 
     interm = m.features(x)
-    interm = linearize_tensor(interm)
+    interm = reshape(interm, :, size(interm, ndims(interm)))
     return m.classifier(interm)
 end
+
+struct Flatten
+    has_codes::Union{Bool, Nothing}
+end
+
+Flatten() = Flatten(nothing)
+
+Flux.@functor Flatten
+(m::Flatten)(x) = reshape(x, :, size(x, ndims(x)))
 
 function test(model, data_loader; criterion=Flux.Losses.logitcrossentropy, label="")
     test_loss, correct = 0.0, 0
